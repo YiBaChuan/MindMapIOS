72a6b00f57275a6edf652acae2fd4d881e200529
Леха <lesha@macbook-pro-leha.local>
2016-04-08 06:30:05 -0700

    Initial Commit

---
+++ mindMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mindMap
+//
+//  Created by Леха on 08.04.16.
+//  Copyright © 2016 Леха. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mindMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mindMap
+//
+//  Created by Леха on 08.04.16.
+//  Copyright © 2016 Леха. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
